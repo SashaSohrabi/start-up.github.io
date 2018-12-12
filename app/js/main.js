00b26f83340cf064c4acd541efc9a4257437d27e
sashasohrabi <shb.sohrabi@gmail.com>
2018-12-13 00:09:49 +0200

    hamburger-menu modified.

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,52 @@
 // ============ HAMBURGER MENU ============
-$(document).ready(function() {
-  $(".fa-bars").click(function() {
+(function() {
+  $(".menu-wrapper").on("click", function() {
+    $(".hamburger-menu").toggleClass("animate");
     $(".header__menu").toggleClass("open");
   });
-});
+})();
 
 // ============ SLICK ============
-$(".about__slide").slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false
+(function() {
+  $(".about__slide").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
-$(".brands__slide").slick({
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  arrows: false,
-  autoplaySpeed: 2000
-});
+    ]
+  });
+  $(".brands__slide").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000
+  });
+})();
 
 // ============ FORM VALIDATION ============
 (function() {
